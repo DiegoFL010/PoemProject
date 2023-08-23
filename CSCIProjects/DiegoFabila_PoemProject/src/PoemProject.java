4b267214ff47582b8a452d43abbb715beac64b8d
DiegoFL010 <143002266+DiegoFL010@users.noreply.github.com>
2023-08-23 18:16:40 -0500

    Add files via upload

---
+++ CSCIProjects/DiegoFabila_PoemProject/src/PoemProject.java
@@ -0,0 +1,12 @@
+/** Poem Project
+ * Diego Fabila Lopez
+ * Fall 2023
+ * CSci 1130
+ */
+public class PoemProject {
+    public static void main(String []args){
+System.out.println("Hello World");
+
+    }
+}
+
